e69f5d14f13bea575477f3179cde030ecd6f5002
KawenjaVerse <piuskawenja58@gmail.com>
2025-11-03 14:17:08 +0300

    terms. and policy

--- lib/Home/TabWidgets/AchievementsWidget.dart
+++ lib/Home/TabWidgets/AchievementsWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nrm_afrosoft_flutter/Home/TabWidgets/AchievementsDetail.dart';
 
 import '../../Utils/Constants.dart';
 import '../../Utils/Helper.dart';
@@ -6,30 +7,35 @@ import '../../Utils/Helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class AchievementsWidget extends StatefulWidget{
+class AchievementsWidget extends StatefulWidget {
   @override
   State<AchievementsWidget> createState() {
     return _AchievementsWidget();
   }
-
 }
 
-class _AchievementsWidget extends State<AchievementsWidget>{
-
+class _AchievementsWidget extends State<AchievementsWidget> {
   var _loading_achievements = false;
   var _achievements = [];
   void get_achievements() {
-    requestAPI(getApiURL("retrieve_all_achievements.php"), {"":""}, (loading){
-      setState(() {
-        _loading_achievements = loading;
-      });
-    }, (response){
-      print("__achievements");
-      setState(() {
-        _achievements = response;
-      });
-      print(_achievements);
-    }, (error){}, method: "GET");
+    requestAPI(
+      getApiURL("retrieve_all_achievements.php"),
+      {"": ""},
+      (loading) {
+        setState(() {
+          _loading_achievements = loading;
+        });
+      },
+      (response) {
+        print("__achievements");
+        setState(() {
+          _achievements = response;
+        });
+        print(_achievements);
+      },
+      (error) {},
+      method: "GET",
+    );
   }
 
   @override
@@ -37,74 +43,87 @@ class _AchievementsWidget extends State<AchievementsWidget>{
     super.initState();
     get_achievements();
   }
+
   @override
   Widget build(BuildContext context) {
-
     return SingleChildScrollView(
       padding: const EdgeInsets.all(16.0),
       child:
-      _loading_achievements ? bossBaseLoader() :
-      Column(
-        children:
-        _achievements.map((achievement) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            elevation: 4,
-            margin: const EdgeInsets.only(bottom: 20),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                children: [
-                  // Image
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.network(
-                      getImageURL("AchievementImages", achievement["image"]),
-                      height: 180,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-
-                  // Title Text
-                  Text(
-                    achievement["title"]!,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-
-                  // Centered Round Yellow Button
-                  Center(
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.amber,
-                        shape: BoxShape.circle,
-                      ),
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.arrow_forward,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          // TODO: Add action here (e.g., open details)
+          _loading_achievements
+              ? bossBaseLoader()
+              : Column(
+                children:
+                    _achievements.map((achievement) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) => const AchievementsDetail(),
+                            ),
+                          );
                         },
-                      ),
-                    ),
-                  ),
-                ],
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          elevation: 4,
+                          margin: const EdgeInsets.only(bottom: 20),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: [
+                                // Image
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: Image.network(
+                                    getImageURL(
+                                      "AchievementImages",
+                                      achievement["image"],
+                                    ),
+                                    height: 180,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+
+                                // Title Text
+                                Text(
+                                  achievement["title"]!,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                const SizedBox(height: 12),
+
+                                // Centered Round Yellow Button
+                                Center(
+                                  child: Container(
+                                    decoration: const BoxDecoration(
+                                      color: Colors.amber,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: IconButton(
+                                      icon: const Icon(
+                                        Icons.arrow_forward,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: () {
+                                        // TODO: Add action here (e.g., open details)
+                                      },
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
               ),
-            ),
-          );
-        }).toList(),
-      ),
     );
   }
 }
-
